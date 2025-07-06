9b8a4cf7ba466e75929296f077dc948e1715c4cc
Tally <113806118+thenatally@users.noreply.github.com>
2025-07-05 21:40:39 -0400

    start

--- src/main/java/tally/scratchcodersmod/SCODiscordAuth.java
+++ src/main/java/tally/scratchcodersmod/SCODiscordAuth.java
@@ -1,16 +1,13 @@
 package tally.scratchcodersmod;
 
-import net.fabricmc.api.ModInitializer;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import net.fabricmc.api.ModInitializer;
+
 public class SCODiscordAuth implements ModInitializer {
 	public static final String MOD_ID = "sco-discord-auth";
-
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
+	
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
